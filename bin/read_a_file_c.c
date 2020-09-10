af11195a5b97e1ece8bfceef01ea1f401faad208
Davide Castellani <38205382+DavideC03@users.noreply.github.com>
2020-09-10 13:30:45 +0200

    Created bin/template.c

---
+++ bin/read_a_file_c.c
@@ -0,0 +1,22 @@
+/**
+ * @file read_a_file_c.cpp
+ *
+ * @version 01.01 2020-08-15
+ *
+ * @brief Read a file and write the content in the terminal
+ *
+ * @ingroup read_a_file_c
+ * (Note: this needs exactly one @defgroup somewhere)
+ *
+ * @author Castellani Davide
+ *
+ * Contact: contacts@castellanidavide.it
+ *
+ */
+#include <stdio.h>
+
+int main() {
+	// printf() displays the string inside quotation
+	printf("read_a_file_c");
+	return 0;
+}
